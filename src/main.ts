830d2033f202b90ff4633232adf55a4eed13a842
Abdel Hajou <a.hajou@avisi.nl>
2021-02-18 22:54:33 +0100

    Replaced env variables with values

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,27 @@ Vue.config.productionTip = false
 // Composition API
 Vue.use(VueCompositionAPI)
 
+// var firebaseConfig = {
+//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
+//   databaseURL: "https://" + process.env.VUE_APP_FIREBASE_DB_SITE + ".firebasedatabase.app",
+//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
+//   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.VUE_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+// }
 var firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
-  databaseURL: "https://" + process.env.VUE_APP_FIREBASE_DB_SITE + ".firebasedatabase.app",
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
-}
+  apiKey: "AIzaSyAuNbkFjY_iI3QRncvMUHvYQqH15N8ZvQ4",
+  authDomain: "go-guess-841d7.firebaseapp.com",
+  databaseURL: "https://go-guess-841d7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "go-guess-841d7",
+  storageBucket: "go-guess-841d7.appspot.com",
+  messagingSenderId: "605422892425",
+  appId: "1:605422892425:web:1b9f062f829c72b63d5d58",
+  measurementId: "G-8T88K514L0"
+};
+
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 firebaseApp.analytics()
